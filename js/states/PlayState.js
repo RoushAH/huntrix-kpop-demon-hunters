7487bf9c1d55de6ee5d9949a316f965fdfbb4df6
RoushAH <Roush.Adam.H@gmail.com>
2026-07-01 15:45:53 +0100

    Add defeated flag and proper heal amounts to health pills

--- js/states/PlayState.js
+++ js/states/PlayState.js
@@ -286,7 +286,7 @@ export class PlayState extends BaseState {
       const pill = new HealthPill(
         enemy.position.x + enemy.size.x / 2 - 8,
         enemy.position.y + enemy.size.y / 2 - 8,
-        0
+        healAmount
       );
       this.healthPills.push(pill);
     }
